11b9d31c246fd203d1f50012ca015db7b49885d4
andrew stehno <andrew_stehno@yahoo.com>
2019-11-11 18:19:24 -0700

    created express routes

---
+++ server.js
@@ -0,0 +1,18 @@
+// Dependencies:=========================
+const express = require("express");
+const path = require("path");
+
+// Express configuration:================
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// Listener:==============================
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
