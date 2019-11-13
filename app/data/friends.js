9ae99e397aff987cbc5d82a9d5f57784c1b9470a
andrew stehno <andrew_stehno@yahoo.com>
2019-11-13 16:55:34 -0700

    added modals to survey.html and began logic for best match.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,5 @@
 const members = [{name:'Bob', photo:'www.goolge.com', scores:Array.from({length:10}, () => 1)},
+{name:'Dwight', photo:'www.googlee.com', scores:Array.from({length:10}, () => 3)},
  {name:'Crystal', photo:'www.gmail.com', scores:Array.from({length:10}, () => 5)}];
 
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,29 +1,41 @@
 const friendsData = require("../data/friends.js");
 
+
 // Routing:==============================
-module.exports = function(app) {
+module.exports = function (app) {
 
-app.get ("/api/friends", function(req, res) {
-    res.json(friendsData);
-});
+    app.get("/api/friends", function (req, res) {
+        res.json(friendsData);
+    });
 
-app.post("/api/friends", function(req, res) {
-    //pon req.body we have the structure of a `friend`
-    //we want to iterate through stored members AKA friendsData
-    //We want to look at each friend and then iterate through each friends scores
-    //Look at each individual score and match it up with the index position 
-    //of the incoming scores IE user[0].scores[0] should match up to same question as incomingScores[0]
-    //we should take the abs value of the subtraction of the two to get a difference
-    //All the while in the outer loop we need a counter varibale keeping track of the total difference for each inner loop
-    //So then you check to see is the total difference less than your current lowest
-    
+    app.post("/api/friends", function (req, res) {
+        //Upon req.body we have the structure of a `friend`
+        //we want to iterate through stored members AKA friendsData
+        // for (let i = 0; i < friendsData.length; i++) {
+        //     // let friends = friendsData[i];
+        //     // console.log(friends);
+
+        //     let scoreCounter = 0;
+        //     //We want to look at each friend and then iterate through each friends scores
+        //     for (let j = 0; j < friendsData[i].scores[j]; j++) {
+        //         scoreCounter += Math.abs(friendsData[i].scores[j] - friendsData.scores[j]);
+        //         // console.log(scoreCounter);
+        //     }
+
+        // }
+        //Look at each individual score and match it up with the index position 
+        //of the incoming scores IE user[0].scores[0] should match up to same question as incomingScores[0]
+        //we should take the abs value of the subtraction of the two to get a difference
+        //All the while in the outer loop we need a counter varibale keeping track of the total difference for each inner loop
+        //So then you check to see is the total difference less than your current lowest
 
 
 
 
-    friendsData.push(req.body);
-      res.json(friendsData);
-})
+
+        friendsData.push(req.body);
+        res.json(friendsData);
+    })
 
 };
 
