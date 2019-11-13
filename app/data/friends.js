3dd0515a0c346437d9eabe90c4d424c8ad27a682
andrew stehno <andrew_stehno@yahoo.com>
2019-11-12 21:12:05 -0700

    changed Jquery cdn from slim, to min, to correct issue with post/ajax.  psuedo coded match functioning.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,9 @@
-const members = [];
+const members = [{name:'Bob', photo:'www.goolge.com', scores:Array.from({length:10}, () => 1)},
+ {name:'Crystal', photo:'www.gmail.com', scores:Array.from({length:10}, () => 5)}];
+
+
+
+
+module.exports = members;
 
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,8 +8,21 @@ app.get ("/api/friends", function(req, res) {
 });
 
 app.post("/api/friends", function(req, res) {
+    //pon req.body we have the structure of a `friend`
+    //we want to iterate through stored members AKA friendsData
+    //We want to look at each friend and then iterate through each friends scores
+    //Look at each individual score and match it up with the index position 
+    //of the incoming scores IE user[0].scores[0] should match up to same question as incomingScores[0]
+    //we should take the abs value of the subtraction of the two to get a difference
+    //All the while in the outer loop we need a counter varibale keeping track of the total difference for each inner loop
+    //So then you check to see is the total difference less than your current lowest
+    
+
+
+
+
     friendsData.push(req.body);
-      res.json(true);
+      res.json(friendsData);
 })
 
 };
